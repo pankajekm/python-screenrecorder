76df6aa80eb234d784e54c70ef0dc8e61cc0d60f
RoranBlackfire <memotoskbhat@gmail.com>
2020-10-04 11:03:14 +0530

    screen capture class created

--- resources/screencapture.py
+++ resources/screencapture.py
@@ -2,21 +2,28 @@ import cv2
 import numpy as np
 import os
 import pyautogui
-output = "video.avi"
-img = pyautogui.screenshot()
-img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-height, width, channels = img.shape
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
 
-while(True):
- try:
-  img = pyautogui.screenshot()
-  image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-  out.write(image)
-  StopIteration(0.5)
- except KeyboardInterrupt:
-  pass
+class WriteVideoToFile(cv2.VideoWriter):
+  # def __init__(self, filename, fourcc, fps, framesize):
+  #   calls the super class __init__ function
+  def __enter__(self):
+    return self
 
-out.release()
-cv2.destroyAllWindows()
+  def __exit__(self, type, value, traceback):
+    self.release()
+
+class ScreenCapture:
+  def __init__(self):
+    self.FOURCC = cv2.VideoWriter_fourcc(*'mp4v')
+    img = pyautogui.screenshot()
+    self.height, self.width, self.channels = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR).shape
+
+  def recordScreen(self, filename):
+    with WriteVideoToFile(filename, self.FOURCC, 20.0, (self.width, self.height)) as out:
+      while(True):
+        try:
+          image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR)
+          out.write(image)
+          StopIteration(0.5)
+        except KeyboardInterrupt:
+          pass
